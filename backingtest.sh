cff58564008dd3771fbe2d43268c30b1e1846058
jimao <jimao@jimaodeMBP.lan>
2022-03-24 20:39:18 +0800

    回测脚本更新

--- backingtest.sh
+++ backingtest.sh
@@ -3,7 +3,8 @@ cd ft_userdata
 docker-compose run --rm freqtrade backtesting \
   --config user_data/config.json \
   --strategy SampleStrategy \
-  --timeframe 1d \
+  --timeframe 5m \
   --pairs BTC/USDT \
   --dry-run-wallet 1000 \
+  --breakdown day month \
   > report.txt
