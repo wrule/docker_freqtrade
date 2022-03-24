9d939107049a757c4a34ddb1941c9b347de101b8
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-24 11:48:36 +0800

    回测数据

--- backingtest.sh
+++ backingtest.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo backingtest
+cd ft_userdata
+docker-compose run --rm freqtrade backtesting \
+  --config user_data/config.json \
+  --strategy SampleStrategy \
+  -i 1m \
+  -p BTC/USDT
