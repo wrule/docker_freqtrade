cf1d25d7c3b978d4f043ed4fbdc81423c705be72
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-28 12:49:31 +0800

    测试策略

--- backingtest.sh
+++ backingtest.sh
@@ -2,7 +2,7 @@
 cd ft_userdata
 docker-compose run --rm freqtrade backtesting \
   --config user_data/config.json \
-  --strategy hlhb \
+  --strategy MultiMa \
   --timeframe 15m \
   --pairs BTC/USDT \
   --dry-run-wallet 1000 \
