32ccce9ddeb43627c4fcfe1d5f97c491fc843a00
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-30 19:56:22 +0800

    提交

--- ft_userdata/user_data/strategies/jimao/srsi_crossed.py
+++ ft_userdata/user_data/strategies/jimao/srsi_crossed.py
@@ -90,10 +90,15 @@ class SRSICrossed(IStrategy):
     def informative_pairs(self):
       return []
 
-    buy_length = IntParameter(49 - 3, 49 + 3, default = 49)
-    buy_rsi_length = IntParameter(8 - 3, 8 + 3, default = 8)
-    buy_k = IntParameter(8 - 3, 8 + 3, default = 8)
-    buy_d = IntParameter(27 - 3, 27 + 3, default = 27)
+    buy_length = IntParameter(49, 49 + 1, default = 49)
+    buy_rsi_length = IntParameter(8, 8 + 1, default = 8)
+    buy_k = IntParameter(8, 8 + 1, default = 8)
+    buy_d = IntParameter(27, 27 + 1, default = 27)
+
+    r_length = 49
+    r_rsi_length = 8
+    r_k = 8
+    r_d = 27
 
     def populate_indicators(
       self,
