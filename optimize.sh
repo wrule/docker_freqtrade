c1ff9d7f30218706d995c24063967bcc15861729
jimao <jimao@jimaodeMBP.lan>
2022-04-04 22:10:13 +0800

    提交

--- optimize.sh
+++ optimize.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd ft_userdata
 docker-compose run --rm freqtrade hyperopt \
-  --hyperopt-loss MaxDrawDownHyperOptLoss \
-  --spaces roi \
+  --hyperopt-loss OnlyProfitHyperOptLoss \
+  --spaces buy \
   --timeframe 2h \
   --pairs BTC/USDT \
   --fee 0.0015 \
   --strategy-path user_data/strategies/jimao \
-  --strategy SRSICrossedR \
+  --strategy SRSICrossed \
   --dry-run-wallet 100 \
   --config user_data/config.json \
   --epochs 10000000 \
