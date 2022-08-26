f1af171f88ad905ee988c03c1219bbdc26a0e899
jimao <jimao@jimaodeMacBook-Pro.local>
2022-08-26 23:04:13 +0800

    下载

--- download-data.sh
+++ download-data.sh
@@ -3,5 +3,5 @@ cd ft_userdata
 docker-compose run --rm freqtrade download-data \
   --exchange binance \
   --days 5000 \
-  --timeframes 1m 5m 15m 30m 1h 2h 6h 12h 1d \
-  --pairs-file user_data/data/binance/pairs2.json
+  --timeframes 1m 5m 15m 30m 1h 2h 4h 6h 12h 1d \
+  --pairs-file user_data/data/binance/pairs.json
